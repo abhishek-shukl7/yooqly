fdd80f1d3b31edd28189d3020ad396dc6a625f16
Abhishek Shukla <abhishekshukla12897@gmail.com>
2025-09-07 21:39:45 +0530

    Initial commit for CRM project

--- crm/config/redis.js
+++
@@ -1,9 +0,0 @@
-// config/redis.js
-const redis = require('redis');
-
-const client = redis.createClient(); // default localhost:6379
-
-client.on('error', err => console.error('Redis error:', err));
-client.connect(); // for redis v4+
-
-module.exports = client;
