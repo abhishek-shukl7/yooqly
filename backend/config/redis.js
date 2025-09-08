943ca2e4c4921dbb377bfdc96eb8f9c8364a33c4
Abhishek Shukla <abhishekshukla12897@gmail.com>
2025-09-08 12:55:14 +0530

    redis changes

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,7 +1,9 @@
 // config/redis.js
 const redis = require('redis');
 
-const client = redis.createClient(); // default localhost:6379
+const client = redis.createClient({
+    url: process.env.REDIS_URL
+}); // default localhost:6379
 
 client.on('error', err => console.error('Redis error:', err));
 client.connect(); // for redis v4+
