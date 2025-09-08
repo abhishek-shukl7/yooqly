eabd13d0c99f51dc6ec87204cb5a71011b678b61
Abhishek Shukla <abhishekshukla12897@gmail.com>
2025-09-08 12:30:01 +0530

    db.js changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 function connectDB(){
-    mongoose.connect(process.env.MONGO_URI).then(() => {
+    mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
         console.log('Mongo DB Connected');
     }).catch(err => {
         console.log('MongoDB error',err);
