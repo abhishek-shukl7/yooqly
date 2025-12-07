1992ba29b0fb8c61236b6bdb0cc7943441b78e0f
Abhishek Shukla <abhishekshukla12897@gmail.com>
2025-12-07 17:15:07 +0530

    added job type api

--- backend/models/jobTypeModel.js
+++ backend/models/jobTypeModel.js
@@ -1,30 +1,25 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+// const ValueSchema = new mongoose.Schema({
+//   type: mongoose.Schema.Types.Mixed // values can be string, number, array, etc.
+// }, { _id: false });
+
 const FieldSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  required: {
-    type: Boolean,
-    default: false
-  },
-  values: {
-    type: [String],
-    default: []
-  }
-});
+  name: { type: String, required: true },
+  required: { type: Boolean, default: false },
+  values: { type: [mongoose.Schema.Types.Mixed], default: [] }
+}, { _id: false });
+
+const TypeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  fields: { type: [FieldSchema], default: [] }
+}, { _id: false });
 
 const jobTypeSchema = new mongoose.Schema({
-  type: {
-    type: String,
-    required: true
-  },
-  fields: {
-    type: [FieldSchema],
-    default: []
-  }
+  name: { type: String, required: true },
+  type: { type: [TypeSchema], default: [] },
+  fields: { type: [FieldSchema], default: [] }
 }, { timestamps: true });
 
 const jobTypeModel = mongoose.model('Jobtype', jobTypeSchema);
