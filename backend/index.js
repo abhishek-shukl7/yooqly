648799368e9fcee72c13fd0b608fefc11b31ae94
Abhishek Shukla <abhishekshukla12897@gmail.com>
2025-12-14 16:00:12 +0530

    production api

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,26 @@ const authRoutes = require('./routes/authRoutes');
 const onboardingRoutes = require('./routes/onboardingRoutes');
 
 connectDB();
-app.use(cors());
+
+const allowedOrigins = [
+  'https://stage-bigdaddy.hackersdaddy.com',
+  'http://localhost:3000'
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
